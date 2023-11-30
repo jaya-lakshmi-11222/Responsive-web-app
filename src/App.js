677d0781c4edfc894487865b21f28e959fded9c4
jaya-lakshmi-11222 <105057158+jaya-lakshmi-11222@users.noreply.github.com>
2023-11-30 19:13:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import Nav from './components/nav';
+import Techno from './components/tech';
+import Home from './components/home';
+import Footer from './components/footer';
+import Card from './components/card';
+
+function App() {
+  return (
+    <>
+    <div className='main'>
+      <Nav/>
+    <Techno/>
+   <Home/>
+    <Footer/>
+    <Card/>
+    </div>
+    </>
+  );
+}
+
+export default App;
